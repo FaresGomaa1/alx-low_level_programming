d7a68d9e11d22dfb627c1d072cc80bfdfc7ac808
FaresGomaa1 <faresgomaa336@gmail.com>
2023-10-05 11:16:28 -0700

    done

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,4 @@
-i#include <stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -8,6 +8,7 @@ i#include <stdio.h>
 int main(void)
 {
 printf("Size of a char: %zu byte(s)\n", sizeof(char));
+
 printf("Size of an int: %zu byte(s)\n", sizeof(int));
 printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
