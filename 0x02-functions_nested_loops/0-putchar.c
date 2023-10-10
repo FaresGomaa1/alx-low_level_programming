1084d92b2278a647ba6663faa99a94cf050d2ea1
FaresGomaa1 <faresgomaa336@gmail.com>
2023-10-10 10:08:31 -0700

    done

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+int main(void)
+{
+    char str[] = "_putchar\n";
+    int i = 0;
+
+    while (str[i])
+    {
+        _putchar(str[i]);
+        i++;
+    }
+
+    return (0);
+}
