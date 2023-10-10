1cf0c2963e911f75681437b3b2029e3470b7b34e
FaresGomaa1 <faresgomaa336@gmail.com>
2023-10-10 10:11:49 -0700

    done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,19 @@
 #include "main.h"
-
+/**
+ * main - Entry point for the program
+ * Description: Uses _putchar to print a string
+ * Return: 0 if successful
+ */
 int main(void)
 {
-    char str[] = "_putchar\n";
-    int i = 0;
+	char str[] = "_putchar\n";
+	int i = 0;
 
-    while (str[i])
-    {
-        _putchar(str[i]);
-        i++;
-    }
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
 
-    return (0);
+	return (0);
 }
