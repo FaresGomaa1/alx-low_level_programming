7a21e9921bdab8ffea87e0707e6725cea3a8c9fd
FaresGomaa1 <faresgomaa336@gmail.com>
2023-10-12 18:17:00 -0700

    done

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -12,6 +12,7 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-
+void _print_str(char str[]);
+void _print_num(int n);
 
 #endif /* MAIN_H */
