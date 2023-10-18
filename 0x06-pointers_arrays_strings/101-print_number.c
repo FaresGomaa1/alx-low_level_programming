fec0bdc80656b60773fff69a5443c269f2f1bf66
FaresGomaa1 <faresgomaa336@gmail.com>
2023-10-18 10:24:14 -0700

    done

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_number - Prints an integer.
+ * print_number - prints an integer.
  * @n: The integer to be printed.
  */
 void print_number(int n)
@@ -13,7 +13,9 @@ void print_number(int n)
 	}
 
 	if (n / 10)
+	{
 		print_number(n / 10);
+	}
 
 	_putchar((n % 10) + '0');
 }
