cfaca4a803bddc91544273eb149046045b95e4e5
FaresGomaa1 <faresgomaa336@gmail.com>
2023-10-30 11:29:25 -0700

    done

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,36 +1,30 @@
 #include "main.h"
+
 /**
  * _strdup - duplicates a string.
  * @str: string to be duplicated.
- *
- * Return: pointer to the newly allocated space in memory,
- *         which contains a copy of the string given as a parameter,
- *         or NULL if the function fails.
+ * Return: pointer to the duplicated string or NULL if failed.
  */
-
 char *_strdup(char *str)
 {
-	char *copied_arr;
-	int arr_len, i;
+	char *duplicate;
+	unsigned int len, i;
 
-	if (str == NULL)
+	if (!str)
 		return (NULL);
 
-	arr_len = 0;
-	while (str[arr_len] != '\0')
-		arr_len++;
+	len = 0;
+	while (str[len])
+		len++;
 
-	if (arr_len == 0)
+	duplicate = malloc((len + 1) * sizeof(char));
+	if (!duplicate)
 		return (NULL);
 
-	copied_arr = malloc(arr_len + 1);
-	if (copied_arr == NULL)
-		return (NULL);
+	for (i = 0; i < len; i++)
+		duplicate[i] = str[i];
 
-	for (i = 0; i < arr_len; i++)
-	copied_arr[i] = str[i];
+	duplicate[len] = '\0';
 
-	copied_arr[arr_len] = '\0';
-
-	return (copied_arr);
+	return (duplicate);
 }
