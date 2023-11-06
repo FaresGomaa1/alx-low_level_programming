b19f70f8aac10b4894618a4c026245bc66c12db7
FaresGomaa1 <faresgomaa336@gmail.com>
2023-11-06 10:54:02 -0800

    done

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: This program prints the name of the file it was compiled from.
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
 	printf(__FILE__ "\n");
