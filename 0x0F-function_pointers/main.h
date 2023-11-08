2f191682ca1228a3ac797a8cee486a79ee23988a
FaresGomaa1 <faresgomaa336@gmail.com>
2023-11-08 13:59:07 -0800

    done

---
+++ 0x0F-function_pointers/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _putchar(char c)
+void print_name_as_is(char *name);
+void print_name_uppercase(char *name);
+int main(void);
+
+#endif /* MAIN_H */
