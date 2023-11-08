50fd73a6350a0d1e241f561e6d4a147cf012787a
FaresGomaa1 <faresgomaa336@gmail.com>
2023-11-08 14:00:09 -0800

    done

--- 0x0F-function_pointers/main.h
+++ 0x0F-function_pointers/main.h
@@ -4,8 +4,6 @@
 #include <stdio.h>
 
 int _putchar(char c)
-void print_name_as_is(char *name);
-void print_name_uppercase(char *name);
-int main(void);
+void print_name(char *name, void (*f)(char *));
 
 #endif /* MAIN_H */
