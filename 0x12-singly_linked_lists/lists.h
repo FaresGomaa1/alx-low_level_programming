4227080f3d7faa72022ce1960416a0f71eab200c
FaresGomaa1 <faresgomaa336@gmail.com>
2024-02-01 09:44:09 -0800

    done

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,20 +4,25 @@
 #include <stddef.h>
 
 /**
-* struct list_s - A structure representing a singly linked list node
-* @str: String data
+* struct list_s - Singly linked list structure
+* @str: String (malloc'ed string)
 * @len: Length of the string
-* @next: Pointer to the next node in the list
+* @next: Points to the next node
 */
 typedef struct list_s
 {
 char *str;
-size_t len;
+unsigned int len;
 struct list_s *next;
 } list_t;
 
-/* Function prototypes */
+/* Task 0 - Print elements of a linked list */
 size_t print_list(const list_t *h);
+
+/* Task 1 - Return the number of elements in a linked list */
 size_t list_len(const list_t *h);
+
+/* Task 2 - Add a new node at the beginning of a list */
+list_t *add_node(list_t **head, const char *str);
 
 #endif /* LISTS_H */
