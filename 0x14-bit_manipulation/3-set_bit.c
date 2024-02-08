840d39edd246cf514f1f057bb04a384970412102
FaresGomaa1 <faresgomaa336@gmail.com>
2024-02-08 14:19:34 -0800

    done

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
-* get_bit - returns the value of a bit at a given index
-* @n: the number to extract the bit from
-* @index: the index of the bit to extract
+* set_bit - sets the value of a bit at a given index to 1
+* @n: pointer to the number to modify
+* @index: the index of the bit to set
 *
-* Return: the value of the bit at index, or -1 if an error occurred
+* Return: 1 if successful, -1 otherwise
 */
-int get_bit(unsigned long int n, unsigned int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
 if (index >= sizeof(unsigned long int) * 8)
 return (-1);
 
-return ((n >> index) & 1);
+*n |= (1UL << index);
+return (1);
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -9,5 +9,4 @@ void print_binary(unsigned long int n);
 int set_bit(unsigned long int *n, unsigned int index);
 
 
-
 #endif /* MAIN_H */
