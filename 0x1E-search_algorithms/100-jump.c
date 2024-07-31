c9901f549b88aa96415cf433ff1eec9595e21598
FaresGomma1 <faresgomaa336@gmail.com>
2024-07-31 11:12:47 -0700

    done

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,44 +1,42 @@
-#include "search_algos.h"
+#include <stdio.h>
 #include <math.h>
+#include "search_algos.h"
 
 /**
- * jump_search - searches for a value in an array of
- * integers using the Jump search algorithm
- *
- * @array: input array
- * @size: size of the array
- * @value: value to search in
- * Return: index of the number
- */
+* jump_search - Searches for a value in a sorted array
+* @array: Pointer to the first element of the array to search in
+* @size: Number of elements in the array
+* @value: Value to search for
+* Return: The first index where value is located
+*/
 int jump_search(int *array, size_t size, int value)
 {
-	int index, m, k, prev;
+if (array == NULL || size == 0)
+return (-1);
 
-	if (array == NULL || size == 0)
-		return (-1);
+size_t step = sqrt(size); /* Jump step size */
+size_t prev = 0;
+size_t curr = 0;
 
-	m = (int)sqrt((double)size);
-	k = 0;
-	prev = index = 0;
+/* Jump in steps */
+while (curr < size && array[curr] < value)
+{
+printf("Value checked array[%zu] = [%d]\n", curr, array[curr]);
+prev = curr;
+curr += step;
+if (curr >= size)
+curr = size - 1; /* Ensure curr doesn't go out of bounds */
+}
 
-	do {
-		printf("Value checked array[%d] = [%d]\n", index, array[index]);
+printf("Value found between indexes [%zu] and [%zu]\n", prev, curr);
 
-		if (array[index] == value)
-			return (index);
-		k++;
-		prev = index;
-		index = k * m;
-	} while (index < (int)size && array[index] < value);
+/* Linear search within the block */
+for (size_t i = prev; i <= curr; i++)
+{
+printf("Value checked array[%zu] = [%d]\n", i, array[i]);
+if (array[i] == value)
+return (i);
+}
 
-	printf("Value found between indexes [%d] and [%d]\n", prev, index);
-
-	for (; prev <= index && prev < (int)size; prev++)
-	{
-		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
-		if (array[prev] == value)
-			return (prev);
-	}
-
-	return (-1);
+return (-1);
 }
